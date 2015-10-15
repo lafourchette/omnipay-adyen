9b29efbdbb0408f5915894b9b01620d880e20c57
Yoan Mayerau <ymayerau@lafourchette.com>
2015-10-15 11:31:49 +0200

    ADYEN-30: update AbstractJsonResponse to use array instead of StdClass

--- src/Omnipay/AdyenApi/Message/AbstractJsonResponse.php
+++ src/Omnipay/AdyenApi/Message/AbstractJsonResponse.php
@@ -15,20 +15,49 @@ abstract class AbstractJsonResponse extends AbstractResponse
      */
     public function __construct(RequestInterface $request, $data)
     {
-        $decodedData = json_decode($data);
-        // If $data is an empty json array json_decode will return an array
-        // instead of an object :/
-        if (!is_object($decodedData)) {
-            $decodedData = new \StdClass();
-        }
+        // Based on http://www.thedarksideofthewebblog.com/les-petits-choses-marrantes-en-php-json_decode/
+        // It's better and more secured to use array than \StdClass
+        $decodedData = json_decode($data, true);
         parent::__construct($request, $decodedData);
     }
 
     /**
-     * @return \StdClass
+     * @return array
      */
     public function getData()
     {
         return $this->data;
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return null|mixed null in case of data value is not defined
+     */
+    public function getDataValue($key)
+    {
+        if (!isset($this->data[$key])) {
+            return null;
+        }
+
+        return $this->data[$key];
+    }
+
+    /**
+     * @param array $keysPath
+     *
+     * @return mixed|null null in case of one of data value is not defined
+     */
+    public function getNestedDataValue(array $keysPath)
+    {
+        $currentData = $this->data;
+        foreach ($keysPath as $key) {
+            if (!isset($currentData[$key])) {
+                return null;
+            }
+            $currentData = $currentData[$key];
+        }
+
+        return $currentData;
     }
 }
---
+++ src/Omnipay/AdyenApi/Tests/Message/AbstractJsonResponseTest.php
@@ -0,0 +1,142 @@
+<?php
+namespace Omnipay\AdyenApi\Tests\Message;
+
+use Omnipay\AdyenApi\Tests\Mock\AbstractJsonResponseTestMock;
+use Omnipay\Common\Message\RequestInterface;
+
+/**
+ * Class AbstractApiRequestTest
+ */
+class AbstractJsonResponseTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var RequestInterface */
+    private $request;
+
+    /**
+     * @{inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->request = $this->prophesize('Omnipay\Common\Message\RequestInterface');
+    }
+
+    /**
+     * @dataProvider getIsSuccess
+     *
+     * @param bool $isSuccess
+     */
+    public function testSuccess($isSuccess)
+    {
+        $abstractJsonResponse = new AbstractJsonResponseTestMock(
+            $this->request->reveal(),
+            json_encode(array('success' => $isSuccess))
+        );
+
+        $this->assertSame(
+            $isSuccess,
+            $abstractJsonResponse->isSuccessful()
+        );
+    }
+
+    /**
+     */
+    public function testGetDataValue()
+    {
+        $testKey = 'test';
+        $testValue = 'ok';
+        $abstractJsonResponse = new AbstractJsonResponseTestMock(
+            $this->request->reveal(),
+            json_encode(array($testKey => $testValue))
+        );
+
+        $this->assertSame(
+            $testValue,
+            $abstractJsonResponse->getDataValue($testKey)
+        );
+    }
+
+    /**
+     */
+    public function testGetDataValueWithUndefinedKey()
+    {
+        $testKey = 'test';
+        $testValue = 'ok';
+        $abstractJsonResponse = new AbstractJsonResponseTestMock(
+            $this->request->reveal(),
+            json_encode(array('plop' => $testValue))
+        );
+
+        $this->assertSame(
+            null,
+            $abstractJsonResponse->getDataValue($testKey)
+        );
+    }
+
+    /**
+     */
+    public function testGetNestedDataValue()
+    {
+        $testKey = 'test1';
+        $testKey2 = 'test2';
+        $testKey3 = 'test3';
+        $testValue = 'ok';
+        $abstractJsonResponse = new AbstractJsonResponseTestMock(
+            $this->request->reveal(),
+            json_encode(
+                array(
+                    $testKey => array(
+                        $testKey2 => array(
+                            $testKey3 => $testValue,
+                        ),
+                    ),
+                )
+            )
+        );
+
+        $this->assertSame(
+            $testValue,
+            $abstractJsonResponse->getNestedDataValue(array($testKey, $testKey2, $testKey3))
+        );
+    }
+
+    /**
+     */
+    public function testGetNestedDataValueWithUndefinedValue()
+    {
+        $testKey = 'test1';
+        $testKey2 = 'test2';
+        $testKey3 = 'test3';
+        $testValue = 'ok';
+        $abstractJsonResponse = new AbstractJsonResponseTestMock(
+            $this->request->reveal(),
+            json_encode(
+                array(
+                    $testKey => array(
+                        'plop' => array(
+                            $testKey3 => $testValue,
+                        ),
+                    ),
+                )
+            )
+        );
+
+        $this->assertSame(
+            null,
+            $abstractJsonResponse->getNestedDataValue(array($testKey, $testKey2, $testKey3))
+        );
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getIsSuccess()
+    {
+        return array(
+            'SUCCESS' => array(true),
+            'FAILURE' => array(false),
+        );
+    }
+}
