939149d46fddf4fb0ca74fa27eb09d81838cbf15
Yoan Mayerau <ymayerau@lafourchette.com>
2015-10-21 12:01:45 +0200

    Adding errors and messages constants

---
+++ src/Omnipay/AdyenApi/Message/Errors/ErrorType.php
@@ -0,0 +1,17 @@
+<?php
+namespace Omnipay\AdyenApi\Message\Errors;
+
+/**
+ * Class ErrorType
+ * provide constant about error type field
+ * @see https://docs.adyen.com/display/TD/Error+response+fields
+ */
+class ErrorType
+{
+    const INTERNAL = 'internal';
+    const VALIDATION = 'validation';
+    const SECURITY = 'security';
+    const CONFIGURATION = 'configuration';
+
+    const UNKNOW = 'unknow';
+}
