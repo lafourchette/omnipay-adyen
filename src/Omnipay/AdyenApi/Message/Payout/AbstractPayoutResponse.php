5027a4d1d5c0a3c9f645b4ece4945444f7bb8a5b
Yoan Mayerau <ymayerau@lafourchette.com>
2015-11-20 16:49:10 +0100

    Adding fields available if payout request is on error

--- src/Omnipay/AdyenApi/Message/Payout/AbstractPayoutResponse.php
+++ src/Omnipay/AdyenApi/Message/Payout/AbstractPayoutResponse.php
@@ -6,7 +6,14 @@ use Omnipay\AdyenApi\Message\AbstractJsonResponse;
 /**
  * Base Adyen Payout Response
  * Provide getter for :
- *  - pspReference
+ *  - status
+ *  - errorCode
+ *  - message
+ *  - errorType
+ *
+ * That are available if request failed
+ *
+ * And getter for pspReference if call is successful
  */
 abstract class AbstractPayoutResponse extends AbstractJsonResponse
 {
@@ -19,5 +26,53 @@ abstract class AbstractPayoutResponse extends AbstractJsonResponse
     public function getTransactionReference()
     {
         return $this->getDataValue('pspReference');
+    }
+
+    /**
+     * Available if call failed
+     * The HTTP response status code.
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->getDataValue('status');
+    }
+
+    /**
+     * Available if call failed
+     * The Adyen code that is mapped to the error message.
+     * @see https://docs.adyen.com/display/TD/Error+response+fields
+     * @see https://docs.adyen.com/pages/viewpage.action?pageId=5376980
+     * @return string
+     */
+    public function getErrorCode()
+    {
+        return $this->getDataValue('errorCode');
+    }
+
+    /**
+     * Available if call failed
+     * The message, a short explanation of the issue.
+     * @see https://docs.adyen.com/display/TD/Error+response+fields
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->getDataValue('message');
+    }
+
+    /**
+     * Available if call failed
+     * The type of error that was encountered.
+     * One of internal/validation/security/configuration
+     * @see https://docs.adyen.com/display/TD/Error+response+fields
+     *
+     * @return string
+     */
+    public function getErrorType()
+    {
+        return $this->getDataValue('errorType');
     }
 }
