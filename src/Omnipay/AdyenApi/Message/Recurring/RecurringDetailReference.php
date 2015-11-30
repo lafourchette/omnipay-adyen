2df44aea13927c22cdc6fb1db9745be66e133dc9
Yoan Mayerau <ymayerau@lafourchette.com>
2015-11-30 21:51:27 +0100

    Adding recurring detail ref LATEST const

---
+++ src/Omnipay/AdyenApi/Message/Recurring/RecurringDetailReference.php
@@ -0,0 +1,10 @@
+<?php
+namespace Omnipay\AdyenApi\Message\Recurring;
+
+/**
+ * Provide api available values regarding recurring detail reference
+ */
+class RecurringDetailReference
+{
+    const LATEST = 'LATEST';
+}
